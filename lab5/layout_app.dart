f461690e90a808787bad0a0b4161c4a4358ec065
Renusree Deekonda <162680295+renusree12@users.noreply.github.com>
2025-04-09 21:07:07 +0530

    Add files via upload

--- lab5/layout_app.dart
+++ lab5/layout_app.dart
@@ -1,1 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const LApp());
+}
+
+class LApp extends StatelessWidget {
+  const  LApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+
+
+          child:GridView.count(
+            crossAxisCount: 2,
+            children: <Widget>[
+              Container(color:Colors.red, child:Text('SINDHU')),
+              Container(color:Colors.brown, child:Text('SINDHU')),
+              Container(color:Colors.yellow, child:Text('SINDHU')),
+              Container(color:Colors.pink, child:Text('SINDHU')),
+
+            ],
+
+          ),
+        ),
+      ),
+    );
+  }
+
+
+}
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const LApp());
+}
+
+class LApp extends StatelessWidget {
+  const  LApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+
+
+          child:GridView.count(
+            crossAxisCount: 2,
+            children: <Widget>[
+              Container(color:Colors.red, child:Text('SINDHU')),
+              Container(color:Colors.brown, child:Text('SINDHU')),
+              Container(color:Colors.yellow, child:Text('SINDHU')),
+              Container(color:Colors.pink, child:Text('SINDHU')),
+
+            ],
+
+          ),
+        ),
+      ),
+    );
+  }
+
+
+}
 
