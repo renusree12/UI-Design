c02b47c7df271a462a564008122aeb06a40fa3a7
Renusree Deekonda <162680295+renusree12@users.noreply.github.com>
2025-04-09 21:08:04 +0530

    Update layout_app.dart

--- lab5/layout_app.dart
+++ lab5/layout_app.dart
@@ -16,44 +16,10 @@ class LApp extends StatelessWidget {
           child:GridView.count(
             crossAxisCount: 2,
             children: <Widget>[
-              Container(color:Colors.red, child:Text('SINDHU')),
-              Container(color:Colors.brown, child:Text('SINDHU')),
-              Container(color:Colors.yellow, child:Text('SINDHU')),
-              Container(color:Colors.pink, child:Text('SINDHU')),
-
-            ],
-
-          ),
-        ),
-      ),
-    );
-  }
-
-
-}
-
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const LApp());
-}
-
-class LApp extends StatelessWidget {
-  const  LApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-
-
-          child:GridView.count(
-            crossAxisCount: 2,
-            children: <Widget>[
-              Container(color:Colors.red, child:Text('SINDHU')),
-              Container(color:Colors.brown, child:Text('SINDHU')),
-              Container(color:Colors.yellow, child:Text('SINDHU')),
-              Container(color:Colors.pink, child:Text('SINDHU')),
+              Container(color:Colors.red, child:Text('First')),
+              Container(color:Colors.brown, child:Text('Second')),
+              Container(color:Colors.yellow, child:Text('Third')),
+              Container(color:Colors.pink, child:Text('Fourth')),
 
             ],
 
