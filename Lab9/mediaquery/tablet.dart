5bd43d1a486ee3c569390b7e2f98fc9b07b105df
Renusree Deekonda <162680295+renusree12@users.noreply.github.com>
2025-06-05 11:39:38 +0530

    Create tablet.dart

---
+++ Lab9/mediaquery/tablet.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class tablet extends StatelessWidget{
+  const tablet({super.key});
+  @override
+  Widget build(BuildContext context){
+    return Row(
+        children:[
+    Container(
+    color:Colors.lime,
+    child:Text("Tablet"),
+       )
+      ],
+    );
+  }
+}
